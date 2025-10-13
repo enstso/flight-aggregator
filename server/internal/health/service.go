36714cd5138af240f25e9daf4cebfb3ff12638ee
Enstso <enstso@outlook.fr>
2025-10-13 14:10:39 +0200

    implementation .env and health handler

--- server/internal/health/service.go
+++ server/internal/health/service.go
@@ -2,12 +2,14 @@ package health
 
 import "net/http"
 
+// HealthOk creates and returns a pointer to a Health instance with status 200 and message "Health Ok".
 func HealthOk() *Health {
 	health := NewHealth(http.StatusOK, "Health Ok")
 	return health
 }
 
+// HealthNOk creates and returns a pointer to a Health instance with status 503 and message "Health Not Ok".
 func HealthNOk() *Health {
-	health := NewHealth(http.StatusInternalServerError, "Health Not Ok")
+	health := NewHealth(http.StatusServiceUnavailable, "Health Not Ok")
 	return health
 }
--- server/main.go
+++ server/main.go
@@ -4,9 +4,17 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+	"aggregator/internal/config"
+	"aggregator/internal/health"
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("starting point !")
+	//load the config .env
+	config.Load()
+
+	http.HandleFunc("/health", health.HealthHandler)
+	fmt.Println("Server running on :8080")
+	fmt.Println(http.ListenAndServe(":8080", nil))
 }
