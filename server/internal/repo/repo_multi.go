821eaf313308557993fcc4e6c99bb75767fb1e8a
Enstso <enstso@outlook.fr>
2025-10-30 21:06:15 +0100

    implementation FindByPassenger, FindByDestination, FindByPrice methods to multi repo

--- server/internal/repo/repo_multi.go
+++ server/internal/repo/repo_multi.go
@@ -74,17 +74,70 @@ func (m *Multi) FindByNumber(ctx context.Context, id string) (domain.Flight, err
 }
 
 func (m *Multi) FindByPassenger(ctx context.Context, passengerName string) ([]domain.Flight, error) {
-	//TODO Not implemented
-	panic("Not implemented")
+	var lastErr error
+	for _, r := range m.repos {
+		f, err := r.FindByPassenger(ctx, passengerName)
+		if err != nil {
+			if errors.Is(err, domain.ErrFlightNotFound) {
+				lastErr = err
+				continue
+			}
+			return domain.Flights{}, err
+		}
+		if len(f) == 0 {
+			lastErr = domain.ErrFlightsNotFound
+			continue
+		}
+		return f, nil
+	}
+	if lastErr == nil {
+		lastErr = domain.ErrFlightsNotFound
+	}
+	return domain.Flights{}, lastErr
 }
 
 func (m *Multi) FindByDestination(ctx context.Context, departure, arrival string) ([]domain.Flight, error) {
-	//TODO Not implemented
-
-	panic("Not implemented")
+	var lastErr error
+	for _, r := range m.repos {
+		f, err := r.FindByDestination(ctx, departure, arrival)
+		if err != nil {
+			if errors.Is(err, domain.ErrFlightNotFound) {
+				lastErr = err
+				continue
+			}
+			return domain.Flights{}, err
+		}
+		if len(f) == 0 {
+			lastErr = domain.ErrFlightsNotFound
+			continue
+		}
+		return f, nil
+	}
+	if lastErr == nil {
+		lastErr = domain.ErrFlightsNotFound
+	}
+	return domain.Flights{}, lastErr
 }
 
 func (m *Multi) FindByPrice(ctx context.Context, price float64) ([]domain.Flight, error) {
-	//TODO Not implemented
-	panic("Not implemented")
+	var lastErr error
+	for _, r := range m.repos {
+		f, err := r.FindByPrice(ctx, price)
+		if err != nil {
+			if errors.Is(err, domain.ErrFlightNotFound) {
+				lastErr = err
+				continue
+			}
+			return domain.Flights{}, err
+		}
+		if len(f) == 0 {
+			lastErr = domain.ErrFlightsNotFound
+			continue
+		}
+		return f, nil
+	}
+	if lastErr == nil {
+		lastErr = domain.ErrFlightsNotFound
+	}
+	return domain.Flights{}, lastErr
 }
