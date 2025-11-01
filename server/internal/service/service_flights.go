82eac505669df017aea82c35a5ed5464988dff86
Enstso <enstso@outlook.fr>
2025-11-01 13:54:03 +0100

    implement GetFlightsSorted endpoint and sorting functionality by price, time, and departure date

--- server/internal/service/service_flights.go
+++ server/internal/service/service_flights.go
@@ -1,1 +1,61 @@
 package service
+
+import (
+	"aggregator/internal/domain"
+	"aggregator/internal/repo"
+	"context"
+	"sort"
+	"time"
+)
+
+func SortByPrice(ctx context.Context, r *repo.Multi) (domain.Flights, error) {
+	var sortFlights, err = r.List(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	sort.Slice(sortFlights, func(i, j int) bool {
+		return sortFlights[i].Total.Amount <
+			sortFlights[j].Total.Amount
+	})
+	return sortFlights, nil
+}
+
+func SortByTimeTravel(ctx context.Context, r *repo.Multi) (domain.Flights, error) {
+	flights, err := r.List(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	sort.Slice(flights, func(i, j int) bool {
+		durationI := totalTravelTime(flights[i])
+		durationJ := totalTravelTime(flights[j])
+		return durationI < durationJ
+	})
+	return flights, nil
+}
+
+func SortByDepartureDate(ctx context.Context, r *repo.Multi) (domain.Flights, error) {
+	flights, err := r.List(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	sort.Slice(flights, func(i, j int) bool {
+		if len(flights[i].Segments) == 0 || len(flights[j].Segments) == 0 {
+			return false
+		}
+		return flights[i].Segments[0].DepartTime.Before(flights[j].Segments[0].DepartTime)
+	})
+
+	return flights, nil
+}
+
+func totalTravelTime(f domain.Flight) time.Duration {
+	if len(f.Segments) == 0 {
+		return 0
+	}
+	firstDepart := f.Segments[0].DepartTime
+	lastArrive := f.Segments[len(f.Segments)-1].ArriveTime
+	return lastArrive.Sub(firstDepart)
+}
